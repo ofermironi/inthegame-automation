31fe65f9fcab1dc024ca00eb4a4e5e4ab7c03dfc
ofermi <34236619+ofermi@users.noreply.github.com>
2021-11-08 11:45:07 +0200

    Update main.tf

--- Modules/VirtualMachine/main.tf
+++ Modules/VirtualMachine/main.tf
@@ -23,7 +23,7 @@ resource "azurerm_network_interface" "nic01" {
 
   ip_configuration {
     name                          = "IPConfig1"
-    subnet_id                     = data.azurerm_subnet.vm_subnet.id
+    subnet_id                     = "${var.aks_sub_id}"
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.myterraformpublicip.id
   }
