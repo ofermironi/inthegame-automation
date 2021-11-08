b881c13e8739c19dba7db567a3c9b918e1e34ea9
ofermi <34236619+ofermi@users.noreply.github.com>
2021-11-08 11:36:32 +0200

    Update main.tf

--- Modules/VirtualMachine/main.tf
+++ Modules/VirtualMachine/main.tf
@@ -1,10 +1,20 @@
 
 
-data "azurerm_subnet" "vm_subnet" {
-  name                 = "${var.vm_subnet}"
-  virtual_network_name = "${var.vnet_01_name}"
-  resource_group_name  = "${var.resource_group_name}"
-}
+# data "azurerm_subnet" "vm_subnet" {
+#   name                 = "${var.vm_subnet}"
+#   virtual_network_name = "${var.vnet_01_name}"
+#   resource_group_name  = "${var.resource_group_name}"
+# }
+
+
+# Create public IPs
+resource "azurerm_public_ip" "myterraformpublicip" {
+    name                         = "${var.vm_name}-PublicIP"
+    location                     = "${var.Location}"
+    resource_group_name          = "${var.resource_group_name}"
+    allocation_method            = "Dynamic"
+
+    }
 
 resource "azurerm_network_interface" "nic01" {
   name                = "${var.vm_name}-nic01"
@@ -15,10 +25,13 @@ resource "azurerm_network_interface" "nic01" {
     name                          = "IPConfig1"
     subnet_id                     = data.azurerm_subnet.vm_subnet.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.myterraformpublicip.id
   }
 }
 
-resource "azurerm_virtual_machine" "vm" {
+
+#resource "azurerm_virtual_machine" "vm" {
+resource "azurerm_linux_virtual_machine" "vm" {
   name                  = "${var.vm_name}"
   location              = "${var.Location}"
   resource_group_name   = "${var.resource_group_name}"
@@ -31,26 +44,35 @@ resource "azurerm_virtual_machine" "vm" {
   # Uncomment this line to delete the data disks automatically when deleting the VM
   delete_data_disks_on_termination = true
 
-  storage_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
-    version   = "latest"
-  }
-  storage_os_disk {
+  # storage_image_reference {
+  #   publisher = "MicrosoftWindowsServer"
+  #   offer     = "WindowsServer"
+  #   sku       = "2019-Datacenter"
+  #   version   = "latest"
+  # }
+
+
+  source_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "18.04-LTS"
+        version   = "latest"
+    }
+  
+ 
+  os_disk {
     name              = "${var.vm_name}-osdisk"
     caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "${var.managed_disk_type}"
+    storage_account_type  = "${var.managed_disk_type}"
   }
-  os_profile {
+
     computer_name  = "${var.vm_name}"
     admin_username = "${var.admin_username}"
     admin_password = "${var.admin_password}"
-  }
-  os_profile_windows_config {
-    provision_vm_agent = "true"
-  }
+
+ 
+
+
   boot_diagnostics  {
     enabled     = true
     storage_uri = "" 
