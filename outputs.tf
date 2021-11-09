273d98df29e461408b16abf9ad3d8bf6f273b580
ofermi <34236619+ofermi@users.noreply.github.com>
2021-11-09 16:27:38 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,11 @@
- output "vm_public_ip_address" {
+output "aks_name" {
+  value = "${var.Project}-${var.Customer}-${var.Env}-3-aks"
+  }
+output "resource_group_name" {
+  value = "${var.Project}-${var.Customer}-${var.Env}-rg"
+  } 
+
+output "vm_public_ip_address" {
   value = data.azurerm_public_ip.public_ip.ip_address
   }
 
