e3cd818b1430982a11ada29218bbfd5343bbb9e1
ofermi <34236619+ofermi@users.noreply.github.com>
2021-11-10 15:56:52 +0200

    Delete terraform directory

--- terraform/Modules/BackupVault/main.tf
+++
@@ -1,8 +0,0 @@
-
-
-resource "azurerm_recovery_services_vault" "backupvault" {
-  name                = "${var.backup_vault_name}"
-  location            = "${var.Location}"
-  resource_group_name = "${var.resource_group_name}"
-  sku                 = "Standard"
-}
--- terraform/Modules/ResourceGroup/main.tf
+++
@@ -1,8 +0,0 @@
-
-
-
-resource "azurerm_resource_group" "Resource_Group_01" {
-  name     = "${var.resource_group_name}"
-  location = "${var.Location}"
-}
-
