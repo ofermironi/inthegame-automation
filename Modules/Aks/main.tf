39ef8625b656da31d648bedf32a74aba8e22a085
ofermi <34236619+ofermi@users.noreply.github.com>
2021-11-10 16:31:15 +0200

    Update main.tf

--- Modules/Aks/main.tf
+++ Modules/Aks/main.tf
@@ -83,7 +83,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "nodepool1" {
 
 locals {
   private_dns_zone_name = join(".", slice(split(".", azurerm_kubernetes_cluster.aks.private_fqdn), 1, length(split(".", azurerm_kubernetes_cluster.aks.private_fqdn))))
-  private_dns_zone_id   = "${data.azurerm_subscription.current.id}/resourceGroups/${azurerm_kubernetes_cluster.aks.node_resource_group}/providers/Microsoft.Network/privateDnsZones/${local.private_dns_zone_name}"
+#  private_dns_zone_id   = "${data.azurerm_subscription.current.id}/resourceGroups/${azurerm_kubernetes_cluster.aks.node_resource_group}/providers/Microsoft.Network/privateDnsZones/${local.private_dns_zone_name}"
 }
 
 
